7b4530bf82ef146a44f5ffbee6e78d458ce92190
Gaveen Sooriyaarachchi <149564638+kidhasia@users.noreply.github.com>
2024-06-13 19:15:59 +0530

    Initial Commit

---
+++ scripts/script.js
@@ -0,0 +1,45 @@
+let inputBox = document.getElementById("input-box");
+let listBox = document.getElementById("listContainer");
+
+function addTask(){
+    console.log("Clicked")
+
+    if (inputBox.value === ''){
+        alert("Enter a task to complete!");
+    }
+    else {
+        let li = document.createElement("li");
+        li.innerText = inputBox.value;
+        listBox.appendChild(li);
+        
+        let span = document.createElement("span");
+        span.innerHTML = "\u00d7";
+        li.appendChild(span);
+        
+    }
+    inputBox.value = ''; //Removing entered text from  input text field 
+    saveData()
+
+}
+
+listBox.addEventListener("click", function(e){
+    if(e.target.tagName ==="LI"){
+        e.target.classList.toggle("checked");
+        saveData()
+
+    }
+    else if(e.target.tagName=== "SPAN"){
+        e.target.parentElement.remove();
+        saveData()
+
+    }
+},false)
+
+function saveData(){
+    localStorage.setItem("data",listContainer.innerHTML);
+}
+
+function displayData(){
+    listContainer.innerHTML = localStorage.getItem("data");
+}
+displayData();
